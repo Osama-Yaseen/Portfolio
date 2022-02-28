00613f13a5a6ca5d0c094cf87035cc196e66945a
Osama-Yaseen <65042441+Osama-Yaseen@users.noreply.github.com>
2022-02-28 13:16:41 +0300

    Add files via upload

---
+++ Portfolio/assets/js/contact.js
@@ -0,0 +1,38 @@
+
+$(function () {
+    
+    "use strict";
+
+    // init the validator
+    $('#contact-form').validator();
+    
+    // when the form is submitted
+    $('#contact-form').submit(function (e) {
+        
+            e.preventDefault();
+        
+                 if($('#InputEmail').val()!= '' && $('#InputSubject').val() != '' && $('#InputMessage').val() != ''){
+
+                    Email.send({
+                        Host: "smtp.gmail.com",
+                        Username: $('#InputEmail').val(),
+                        Password: "As1d2f3@",
+                        To: 'osama.shehdeh.yaseen@gmail.com',
+                        From: $('#InputEmail').val(),
+                        Subject: $('#InputSubject').val(),
+                        Body: $('#InputMessage').val(),
+                      
+                      })
+                        .then(function (message) {
+                          alert("Mail has been sent successfully");
+                          location.reload();
+                        });
+
+                 }
+
+                  
+                   
+    })
+});
+
+
